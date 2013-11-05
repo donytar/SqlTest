84fee69cbc213f0bd26775fdfeb8c10f43e0fd1b
donytar <donytar@gmail.com>
2013-11-05 19:39:37 +0300

    тест

--- src/com/donytar/Main.java
+++ src/com/donytar/Main.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
-        /*
+        
         try {
             SqlWorker database = new SqlWorker("jdbc:mysql://localhost/my_db", "root", "Nathyrra");
             //database.addRow("Bobic", "Gringo Dolbig", "dog", "m");
@@ -20,11 +20,11 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        */
+        
         try {
             Runtime.getRuntime().exec("mysqldump -u root -pNathyrra my_db -r //home/donytar/my_db.sql");
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            e.printStackTrace();
         }
 
     }
